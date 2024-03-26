32a7a51f1c764c82db5feab3da9945bc665e5a19
kykruz <k.kruzan@yahoo.com>
2024-03-26 09:31:56 -0500

    DAVE

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -9,3 +9,21 @@
 // Calculate a trip to the moon below
 
 // Print the results of the trip to the moon below
+
+let shuttleName = "Determination";
+let shuttleSpeedMph = 17500;
+let distanceToMars = 225000000;
+let distanceToMoon = 384400;
+let milesPerKm = 0.621;
+
+console.log(typeof shuttleName);
+console.log(typeof shuttleSpeedMph);
+console.log(typeof distanceToMars);
+console.log(typeof distanceToMoon);
+console.log(typeof milesPerKm);
+
+let milesToMars = distanceToMars * milesPerKm;
+let hoursToMars = milesToMars / shuttleSpeedMph; 
+let daysToMars = hoursToMars / 24;
+
+console.log(shuttleName , "will take" , daysToMars , "days to reach The Moon")
